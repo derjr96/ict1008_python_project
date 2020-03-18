51c05a7883c54fe1725fd4e341e941d81c7dc89d
derjr96 <54965311+derjr96@users.noreply.github.com>
2020-03-18 15:41:49 +0800

    debugged lrt algorithm, working on main.py (integration of walk and lrt)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import osmnx as ox
+from flask import Flask, render_template
 import heapq
 import time
 import math
@@ -63,9 +63,6 @@ from osmnx.downloader import overpass_request
 from osmnx.errors import *
 from osmnx.core import consolidate_subdivide_geometry, get_polygons_coordinates
 
-query_str = '[out:json][timeout:180];(relation["network"="Singapore Rail"]["route"="monorail"](1.4011,103.8977,1.4154,103.9231);>;);out;'
-response_json = overpass_request(data={'data': query_str}, timeout=180)
-
 
 def get_node(element):
     """
@@ -119,7 +116,7 @@ def parse_osm_nodes_paths(osm_data):
     return nodes, paths
 
 
-def create_graph(response_jsons, name='unnamed', retain_all=True, bidirectional=False):
+def create_graph(mrt_response_json, name='unnamed', retain_all=True, bidirectional=False):
     """
     Create a networkx graph from Overpass API HTTP response objects.
 
@@ -145,7 +142,7 @@ def create_graph(response_jsons, name='unnamed', retain_all=True, bidirectional=
     # make sure we got data back from the server requests
     elements = []
     # for response_json in response_jsons:
-    elements.extend(response_json['elements'])
+    elements.extend(mrt_response_json['elements'])
     if len(elements) < 1:
         raise EmptyOverpassResponse('There are no data elements in the response JSON objects')
 
@@ -156,7 +153,7 @@ def create_graph(response_jsons, name='unnamed', retain_all=True, bidirectional=
     nodes = {}
     paths = {}
     # for osm_data in response_jsons:
-    nodes_temp, paths_temp = parse_osm_nodes_paths(response_jsons)
+    nodes_temp, paths_temp = parse_osm_nodes_paths(mrt_response_json)
     for key, value in nodes_temp.items():
         nodes[key] = value
     for key, value in paths_temp.items():
@@ -186,6 +183,23 @@ def create_graph(response_jsons, name='unnamed', retain_all=True, bidirectional=
     return G
 
 
+# finding which mrt station is closest to the start/end point
+def mrt_nearnode(srctomrt, nodelist):
+    nearnode = []
+    for k in nodelist:
+        if k.get("railway") == "station" or k.get("railway") == "stop":
+            h = heuristic(latlon(srctomrt, nodelist), latlon(k.get("osmid"), nodelist))
+            heapq.heappush(nearnode, (h, k.get("osmid")))
+    return heapq.heappop(nearnode)
+
+
+# retrieving lat/lon coordinates via OSMID
+def latlon(osmid, nodeList):
+    for k in nodeList:
+        if k[2] == osmid:
+            return k[1], k[0]
+
+
 # calculating heuristic between two lat/lon points
 def heuristic(start, end):
     lat1, lon1 = start[0], start[1]
@@ -202,47 +216,31 @@ def heuristic(start, end):
     return dist
 
 
-# finding which mrt station is closest to the start/end point
-def lrt_nearnode(srctomrt):
-    nearnode = []
-    for k in mrtNodeList:
-        if k.get("railway") == "station" or k.get("railway") == "stop":
-            h = heuristic(latlon(srctomrt), latlon(k.get("osmid")))
-            heapq.heappush(nearnode, (h, k.get("osmid")))
-    return heapq.heappop(nearnode)
-
-
-# retrieving lat/lon coordinates via OSMID
-def latlon(osmid):
-    for k in mrtNodeList:
-        if k.get("osmid") == osmid:
-            return k.get("x"), k.get("y")
+# getting walk to station
+def walktoSt(edge_list, start):
+    stat = []
+    for k in edge_list:
+        h = heuristic(latlon(start, edge_list), latlon(k[0][1], edge_list))
+        if h > 5:
+            heapq.heappush(stat, (h, k[0][1]))
+    return heapq.heappop(stat)[1]
 
 
 # ASTAR ALGORITHM
-def lrt_astar(start_point, end_point, edge_list):
+def astar(start_point, end_point, edge_list, node_list):
     closepath = {}
     path = []
     routeq = []
     finalret = []
-    stat = []
-
-    # finding start station (working)
-    for k in edge_list:
-        h = heuristic(latlon(start_point), latlon(k[0][1]))
-        if h > 5:
-            heapq.heappush(stat, (h, k[0][1]))
-    strt = heapq.heappop(stat)[1]
 
     # pushing start point into heapq queue (heuristic, length(dist), parent(key), current(value))
-    heapq.heappush(routeq, (0, 0, None, strt))
-    closepath[strt] = None
+    heapq.heappush(routeq, (0, 0, None, start_point))
+    closepath[start_point] = None
 
     while True:
         temp = heapq.heappop(routeq)
-
         # check if we reach end point node
-        if temp[3] == end_point or heuristic(latlon(temp[3]), latlon(end_point)) < 20:
+        if temp[3] == end_point:
             path.append(temp[3])
             rear = temp[2]
 
@@ -262,50 +260,65 @@ def lrt_astar(start_point, end_point, edge_list):
                     if i[0][1] in closepath:
                         continue
                     else:
-                        he = heuristic(latlon(i[0][1]), latlon(end_point))
+                        h = heuristic(latlon(i[0][1], node_list), latlon(end_point, node_list))
                         cur_length = i[1].get('length')
                         heapq.heappush(routeq,
-                                       ((he + temp[1] + cur_length), cur_length + temp[1], temp[3], i[0][1]))
+                                       ((h + temp[1] + cur_length), cur_length + temp[1], temp[3], i[0][1]))
                         # adding previous path to close path dict to prevent an infinite loop of short path
                         closepath[i[0][1]] = temp[3]
 
 
-# ------------------- main code ------------------- #
-G_lrt = create_graph(response_json)
+# main code
+punggol = (1.403948, 103.909048)
+distance = 2000
+
+# data creation and storing
+mrt_query_str = '[out:json][timeout:180];(relation["network"="Singapore Rail"]["route"="monorail"](1.4011,103.8977,1.4154,103.9231);>;);out;'
+mrt_response_json = overpass_request(data={'data': mrt_query_str}, timeout=180)
+G_lrt = create_graph(mrt_response_json)
+G_walk = ox.graph_from_point(punggol, distance=distance, truncate_by_edge=True, network_type='walk')
+
+# storing all nodes into a list
+walkNodeList = list(G_walk.nodes.values())
+walkEdgeList = list(G_walk.edges.items())
 mrtNodeList = list(G_lrt.nodes.values())
 mrtEdgeList = list(G_lrt.edges.items())
 
-startosmid = ox.get_nearest_node(G_lrt, (1.4014441, 103.8950046), method='euclidean', return_dist=True)
-endosmid = ox.get_nearest_node(G_lrt, (1.399601, 103.9164448), method='euclidean', return_dist=True)
+# user input (GUI TEAM, user input in text area will be stored here)
+src = "220A Sumang Lane, Singapore 821220"  # punggol will return punggol mrt coordinates
+des = "60 Punggol East, Singapore 828825"  # random hdb
+startpoint = ox.geocode(src)
+endpoint = ox.geocode(des)
 
-# testing algorithmn speed
-start_time = time.time()
-mrtfinal = lrt_astar(lrt_nearnode(startosmid[0])[1], lrt_nearnode(endosmid[0])[1], mrtEdgeList)
-print("--- %s seconds ---" % round((time.time() - start_time), 2))
+startosmid = ox.get_nearest_node(G_walk, startpoint, method='euclidean', return_dist=True)
+endosmid = ox.get_nearest_node(G_walk, endpoint, method='euclidean', return_dist=True)
 
-# calculating estimated time to reach the destination
-totaldist = mrtfinal[1] + (startosmid[1] * 1000) + (endosmid[1] * 1000)
-estwalk = totaldist / (45000 / 60)  # avg mrt speed 45km/hr - 750m per minute
-print("Time: " + str(round(estwalk)) + " minutes" + "\nDistance: " + str(round((totaldist / 1000), 2)) + " km")
+# Walk-LRT connection start
+strtlrt = ox.get_nearest_node(G_lrt, startpoint, method='euclidean', return_dist=True)
+
+#################################################### Above correct ####################################################
+mrtStart = walktoSt(mrtEdgeList, strtlrt[0])
+mrtEnd = mrt_nearnode(endosmid[0], mrtNodeList)[1]
+
+# testing WALK algorithmn
+walkToStation = astar(startosmid[0], mrtStart, walkEdgeList, walkNodeList)
+walkFromStation = astar(mrtEnd[1], endosmid[0], walkEdgeList, walkNodeList)
+
+# testing LRT algorithmn
+mrtfinal = astar(walktoSt(mrtEdgeList, mrtStart[1]), mrtEnd, mrtEdgeList, mrtNodeList)
 
 # plotting map to folium
-m = ox.plot_route_folium(G_lrt, mrtfinal[0], route_color='#00008B', route_width=5, tiles="OpenStreetMap")
-m.save('templates/astar_lrt.html')
+m = ox.plot_route_folium(G_walk, walkToStation[0], route_color='#00FFFF', route_width=5, tiles="OpenStreetMap")
+ox.plot_route_folium(G_walk, walkFromStation[0], route_color='#00FFFF', route_width=5, tiles="OpenStreetMap").add_child(m)
+ox.plot_route_folium(G_lrt, mrtfinal[0], route_color='#FF0000', route_width=5, tiles="OpenStreetMap").add_child(m)
+m.save('templates/astaralgo_walklrt.html')
 
-# start = (103.9024702, 1.4051499)
-# end = (103.9164448, 1.399601)
-#
-# for i in range(0, len(nodeList)):
-#     if (nodeList[i].get("x") == start[0]) and (nodeList[i].get("y") == start[1]):
-#         startosmid = nodeList[i].get("osmid")
-#     if (nodeList[i].get("x") == end[0]) and (nodeList[i].get("y") == end[1]):
-#         endosmid = nodeList[i].get("osmid")
+'''
+FLASK IS HERE FLASK IS HERE FLASK IS HERE FLASK IS HERE
+'''
+app = Flask(__name__)
 
-# # user input (GUI TEAM, user input in text area will be stored here)
-# startstring = "32 Punggol East, Singapore 828824" # punggol will return punggol mrt coordinates
-# endstring = "94 punggol central, Singapore 828724"  #random hdb
-# startpoint = ox.geocode(startstring)
-# endpoint = ox.geocode(endstring)
-#
-# startosmid = ox.get_nearest_node(G_lrt, startpoint, method='euclidean')
-# endosmid = ox.get_nearest_node(G_lrt, endpoint, method='euclidean')
+
+@app.route("/")
+def index():
+    return render_template('index.html')
