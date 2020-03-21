95c69c0868544210c145be395cfaed1533fe82c9
Shiro Neko <meow.yoshino@yahoo.com>
2020-03-21 16:29:31 +0800

    Update Bus Retrieval Time, Time to Plot eta: 50secs

--- scripts/PlotShortestBusRouteHelperBusFINAL.py
+++ scripts/PlotShortestBusRouteHelperBusFINAL.py
@@ -12,25 +12,24 @@ def removeDupes(list):
 
 
 def findPath(paths):
+    startTime = time.time()
+
     api = overpy.Overpass()
     punggol = (1.4041070, 103.9025242)  # Punggol Interchange as mid point
     distance = 3000
     G = ox.graph_from_point(punggol, distance=distance, network_type='drive_service')
 
-    tupleOfStartEnds = []
+    listOfNodes = []
     tupleOfStartEndQueriesNodes = []  # Node IDs of BusStops in [(Start1, End2), (Start2, End3)] format
     route = []  # Routes to travel from [[Route Start1 to End 2], [Route Start2 to End3]] format
 
     for i in range(len(paths)):
-        if i != len(paths) - 1:
-            tupleOfStartEnds.append((paths[i], paths[i + 1]))
+            listOfNodes.append(api.query("node['asset_ref'='" + str(paths[i]) + "'];out;").nodes[0])
 
     # Retriving the long and lat of the nodes using busstop codes
-    for i in range(len(tupleOfStartEnds)):
-        resultStart = api.query("node['asset_ref'='" + str(tupleOfStartEnds[i][0]) + "'];out;")
-        time.sleep(5) #For server load overload prevention
-        resultEnd = api.query("node['asset_ref'='" + str(tupleOfStartEnds[i][1]) + "'];out;")
-        tupleOfStartEndQueriesNodes.append((resultStart.nodes[0], resultEnd.nodes[0]))
+    for i in range(len(listOfNodes)):
+        if i != (len(listOfNodes) - 1):
+            tupleOfStartEndQueriesNodes.append((listOfNodes[i], listOfNodes[i+1]))
 
     # Retriving of all routes(edges) to plot route
     for i in range(len(tupleOfStartEndQueriesNodes)):
@@ -40,5 +39,9 @@ def findPath(paths):
                                                                tupleOfStartEndQueriesNodes[i][1].lon)))[2]))
 
     route = removeDupes(route)
+
+    endTime = time.time()
+
+    print("Bus Route Plotting Takes: ", (endTime - startTime))
 
     return route, tupleOfStartEndQueriesNodes
