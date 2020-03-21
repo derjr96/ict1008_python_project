818e84511d58b03678137e35dca910e8b6f2beb0
derjr96 <54965311+derjr96@users.noreply.github.com>
2020-03-21 16:42:14 +0800

    Organised file directory. Please do not delete any file without asking me. - Darrell

---
+++ astaralgo_walking(not_in_used).py
@@ -0,0 +1,129 @@
+from flask import Flask, render_template
+import folium
+from collections import deque, namedtuple
+import pandas as pd
+import osmnx as ox
+import heapq
+import time
+import math
+
+start_time = time.time()
+starting = time.time()
+ending = time.time()
+
+
+# retrieving lat/lon coords via OSMID
+def latlon(osmid):
+    for i in range(0, len(nodeList)):
+        if nodeList[i].get("osmid") == osmid:
+            lat = nodeList[i].get("x")
+            lon = nodeList[i].get("y")
+    coord = (lat, lon)
+    return coord
+
+
+# calculating heuristic between two lat/lon points
+def heuristic(start, end):
+    lat1, lon1 = start[0], start[1]
+    lat2, lon2 = end[0], end[1]
+    radius = 6371  # km
+
+    distlat = math.radians(lat2 - lat1)
+    distlon = math.radians(lon2 - lon1)
+    a = math.sin(distlat / 2) * math.sin(distlat / 2) + math.cos(math.radians(lat1)) \
+        * math.cos(math.radians(lat2)) * math.sin(distlon / 2) * math.sin(distlon / 2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    dist = radius * c * 1000
+
+    return dist
+
+
+# ASTAR ALGORITHM
+def astar(start_point, end_point):
+    closepath = {}
+    path = []
+    routeQueue = []
+
+    # orginal heuristic distance from start to end point
+    disth = heuristic(latlon(start_point), latlon(end_point))
+    # pushing start point into heapq queue (heuristic, length(dist), parent(key), current(value))
+    heapq.heappush(routeQueue, (0, 0, None, start_point))
+    closepath[start_point] = None
+
+    while True:
+        temp = heapq.heappop(routeQueue)
+        # print(temp[0], temp[1], temp[2])
+        if temp[3] == end_point:
+            path.append(temp[3])
+            rear = temp[2]
+
+            # path list to append all osmid by key in closepath with the first being the end node
+            while rear is not None:
+                path.append(rear)
+                rear = closepath.get(rear)
+            # reverse the path list into start to end
+            path = path[::-1]
+            return path
+
+        for i in range(0, len(edgeList)):
+            if edgeList[i][0][0] == temp[3]:
+                if edgeList[i][0][1] in closepath:
+                    continue
+                else:
+                    h = heuristic(latlon(edgeList[i][0][1]), latlon(end_point))
+                    cur_length = edgeList[i][1].get('length')
+                    if h < disth:
+                        heapq.heappush(routeQueue,
+                                       ((h + temp[1] + cur_length), cur_length + temp[1], temp[3], edgeList[i][0][1]))
+                        # adding previous path to close path dict to prevent an infinite loop of short path
+                        closepath[edgeList[i][0][1]] = temp[3]
+
+
+# main code
+punggol = (1.403948, 103.909048)
+distance = 1500
+G = ox.graph_from_point(punggol, distance=distance, truncate_by_edge=True, network_type='walk')
+
+# # Import to CSV for reference
+# n, e = ox.graph_to_gdfs(G)
+# e.to_csv("edge.csv")
+# n.to_csv("node.csv")
+
+# storing all nodes into a list
+nodeList = list(G.nodes.values())
+edgeList = list(G.edges.items())
+
+# finding start and end point in the list and storing into variables
+start = (103.9073345, 1.4060506)
+end = (103.9172982, 1.3956014)
+
+for i in range(0, len(nodeList)):
+    if (nodeList[i].get("x") == start[0]) and (nodeList[i].get("y") == start[1]):
+        startosmid = nodeList[i].get("osmid")
+    if (nodeList[i].get("x") == end[0]) and (nodeList[i].get("y") == end[1]):
+        endosmid = nodeList[i].get("osmid")
+
+# testing algorithmn speed
+start_time = time.time()
+
+final = astar(startosmid, endosmid)
+print("--- %s seconds ---" % (time.time() - start_time))
+
+# plotting map to folium
+# m = folium.Map(location=punggol)
+m = ox.plot_route_folium(G, final, route_color='#00008B', route_width=5, )
+m.save('templates/astar_walking.html')
+
+'''
+FLASK IS HERE FLASK IS HERE FLASK IS HERE FLASK IS HERE
+'''
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+
+
