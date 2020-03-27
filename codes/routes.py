ab1e80609dba06c5d161572f3edd796604367ad6
thedomilicious <55941185+thedomilicious@users.noreply.github.com>
2020-03-27 20:38:17 +0800

    Merge branch 'master' of D:\Users\domin\Documents\GitHub\ict1008_python_project with conflicts.

--- codes/routes.py
+++ codes/routes.py
@@ -1,17 +1,12 @@
-from flask import render_template, request, redirect, make_response
+from flask import render_template, request, redirect
 from codes import app, G_walk, G_lrt, walkNodeList, walkEdgeList, mrtNodeList, mrtEdgeList
 from codes.walk_astaralgo import AstarWalkAlgo
 from codes.walk_mrt_algo import AstarWalkMrtAlgo
 import codes.PlotShortestWalkBusRoute as DjWalkBus
-from flask_caching import Cache
-
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-store"
-    return response
 
 
 @app.route("/", methods=["GET", "POST"])
+@app.route("/home", methods=["GET", "POST"])
 def home():
     print(request.method)
     if request.method == "POST":
@@ -25,25 +20,32 @@ def home():
             print(dropdown)
             aswa = AstarWalkAlgo(address_input, address_input1, G_walk, walkNodeList, walkEdgeList)
             aswa.generate()
-            # redirect("/walking")
-        elif str(request.form["dropdown"]) == "WalkandMRT":
+            redirect("/walking")
+        if str(request.form["dropdown"]) == "WalkandMRT":
             dropdown = str(request.form["dropdown"])  # dropdown value
             print(address_input, "-->", address_input1)
             print(dropdown)
             asawl = AstarWalkMrtAlgo(address_input, address_input1, G_walk, G_lrt, walkNodeList, walkEdgeList,
                                      mrtNodeList, mrtEdgeList)  # astar walk with mrt
             asawl.generate()
-            # redirect("/walkinglrt")
-        elif str(request.form["dropdown"]) == "WalkandBus":
+            redirect("/walkinglrt")
+        if str(request.form["dropdown"]) == "WalkandBus":
             dropdown = str(request.form["dropdown"])  # dropdown value
             print(address_input, "-->", address_input1)
             print(dropdown)
-            print(type(address_input))
             DjWalkBus.plotShortestWalkBus(address_input, address_input1)
-            # redirect("/walkingbus")
+            redirect("/walkingbus")
+        # if request.form["dropdown"] == "walk":
+        #     address_input = str(request.form["address_input"])  # src
+        #     address_input1 = str(request.form["address_input1"])  # dst
+        #     aswa = AstarWalkAlgo(address_input, address_input1)
+        #     aswa.generate()
+        #     print(address_input, "-->", address_input1)
+        #     redirect("/walking")
 
-        return render_template("base.html")
-    elif request.method == "GET":
+        return render_template('base.html')
+
+    if request.method == "GET":
         return render_template("home.html")
 
 
@@ -56,6 +58,11 @@ def test():
         print(address_input, "-->", address_input1)
 
 
+# @app.route("/walking")
+# def walk():
+#     return render_template("default.html")
+
+
 @app.route("/default")
 def default():
     return render_template("default.html")
@@ -65,3 +72,7 @@ def default():
 def punggol():
     return render_template('punggol.html')
 
+
+# @app.route("/walkinglrt")
+# def walklrt():
+#     return render_template('default.html')
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from codes import app
 
+
 if __name__ == "__main__":
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(debug=False)
+    app.run(debug=True)
